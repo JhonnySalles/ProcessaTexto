efe92eed53b0e872698d8ac7e0bd08deb2a38b3f
JhonnySalles <jhonnysallesnoschag@hotmail.com>
2021-03-24 11:19:26 -0300

    Correção de erro para abertura fora do projeto. Compilado nova versão.

--- TextosJapones/src/main/java/module-info.java
+++ TextosJapones/src/main/java/module-info.java
@@ -13,8 +13,8 @@ module TextosJapones {
 	exports org.jisho.textosJapones;
 	exports org.jisho.textosJapones.model.entities;
 
-	requires transitive com.jfoenix;
 	requires transitive java.sql;
+	requires transitive com.jfoenix;
 	requires javafx.base;
 	requires transitive javafx.controls;
 	requires javafx.fxml;
@@ -22,17 +22,15 @@ module TextosJapones {
 	requires kuromoji.core;
 	requires kuromoji.ipadic;
 	requires transitive sudachi;
-	requires java.desktop;
 	requires AnimateFX;
 	requires transitive org.controlsfx.controls;
-	requires java.compiler;
 	requires org.jsoup;
 	requires com.google.common;
 	requires nativejavafx.taskbar;
 	requires javafx.web;
-	requires json;
 	requires com.google.gson;
-	// requires nativejavafx.taskbar;
+	requires org.json;
+	requires java.desktop;
 
 	opens org.jisho.textosJapones.controller to javafx.fxml, javafx.graphics;
 	opens org.jisho.textosJapones.model.entities to javafx.base;
